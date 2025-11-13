e8249f26f302c24319dff380756ac622a2a8d06c
OwenDeMeyer <daviddemeyer095@gmail.com>
2025-11-13 15:26:50 -0700

    Define base directory and data file paths
    Added base directory and full paths for data files.

--- examples/driver.py
+++ examples/driver.py
@@ -7,6 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from linalg_interp import spline_function  
 
+BASE_DIR = os.path.dirname(__file__)
+
+# full paths to your data files
+water_file = os.path.join(BASE_DIR, 'water_density_vs_temp_usgs.txt')
+air_file = os.path.join(BASE_DIR, 'air_density_vs_temp_eng_toolbox.txt')
 # 1. Load the data from text files
 water_data = np.loadtxt('water_density_vs_temp_usgs.txt')  # two columns: temp, density
 air_data = np.loadtxt('air_density_vs_temp_eng_toolbox.txt')      #two columns: temp, density
